156df80dd7d9e8ed86a6fdc328456aa7d4588301
Yassine Hachguer <yassine.hachguer05@gmail.com>
2025-03-14 14:27:43 +0000

    Création des Interfaces Repositories

---
+++ src/main/java/com/example/user_role/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.user_role.repository;
+
+import com.example.user_role.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByUsername(String username);
+
+}
