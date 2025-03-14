4e89ff5a278816a7001673554e0d66e4aa452c4d
Yassine Hachguer <yassine.hachguer05@gmail.com>
2025-03-14 14:25:21 +0000

    Création des classes User et Role dans le package entities

---
+++ src/main/java/com/example/user_role/entities/User.java
@@ -0,0 +1,27 @@
+package com.example.user_role.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "USERS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String userId;
+    @Column(name = "USER_NAME", unique = true, length = 20)
+    private String username;
+    private String password;
+    @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
+    private List<Role> roles = new ArrayList<>();
+
+
+
+}
